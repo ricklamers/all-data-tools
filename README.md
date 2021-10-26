e4cb126fd918a4cfd923edaa368f22c0ebd9bc47
Rick Lamers <ricklamers@gmail.com>
2021-10-26 14:51:03 +0200

    Initial.

---
+++ README.md
@@ -0,0 +1,86 @@
+# The Data Tools initiative
+Navigating the space of data tools can be daunting. To combat this we need high quality, up-to-date and complete information.
+
+Through this initiative we hope to contribute to a good overview.
+
+### Technology
+The data is stored in the `tools.json` file in this repository which is consumed by a [NocoDB](https://github.com/nocodb/nocodb) instance running on https://alldatatools.com
+
+## Collaboration
+Please add your/your faviorite **data** tool/product to the list! We want this list to be as inclusive as possible.
+
+## Structure
+
+### Entities
+The entities should be either tools or products. The goal is to map all relevant tools a data practitioner might want to use. Both open source and propietary products/tools are welcome. The goal is to be an exhaustive list.
+
+### Licenses
+Licenses should all use the SPDX license format. Find out more on the [LF SPDX licences page](https://spdx.org/licenses/).
+
+### Images
+For logos and screenshots please add a folder to the images root folder in lowercase without spaces (use dashes instead if necessary).
+
+### Taxonomy
+The current taxonomy is hierarchical (some categories are children of others) and non-exclusive. I.e. a tool can be in multiple categories at the same time.
+
+The initial setup is mostly inspired by the [Firstmark MAD Landscape](https://mattturck.com/data2021/). We hope to evolve this taxonomy over time as understanding of the landscape develops and more consensus is built.
+
+**Note: we encourage everyone to discuss this taxonomy through Pull Requests. Please recommend changes if you feel like it will improve the clarity of the overview of tools.**
+
+- Infrastructure
+  - Storage
+  - Data lakes
+  - Data warehouses
+  - Streaming/in-memory
+  - RDBMS
+  - NoSQL databases
+  - NewSQL databases
+  - Real-time databases
+  - Time series database
+  - Graph databases
+  - MPP databases
+  - ETL - ELT - data transformation
+  - Reverse ETL
+  - Data integration
+  - Data governance & access
+  - Privacy and security
+  - Data observability
+  - Data quality
+  - Monitoring
+  - Serverless
+  - Containers
+  - Clusters
+  - Formats
+  - Orchestration
+  - Data versioning
+  - CI/CD
+  - Supply chain security
+  - Package management
+- Analytics
+  - BI platforms
+  - Visualization
+  - Data analyst platforms
+  - Augmented analytics
+  - Data catalog and discovery
+  - Metrics store
+  - Log analytics
+  - Query engine
+  - Search
+- ML & AI
+  - Data Science notebooks
+  - Data Science platforms
+  - ML platforms
+  - Data generation & labeling
+  - Model building
+  - Feature store
+  - Deployment & production
+  - Computer vision
+  - Speech
+  - Drift detection
+  - NLP
+  - Model versioning
+  - Synthetic media
+  - API services
+  - GPU libraries
+
+
---
+++ sync.py
@@ -0,0 +1,216 @@
+import json
+import os
+import argparse
+import logging
+import datetime
+
+import requests
+
+from urllib.parse import urlparse
+from dateutil import parser
+from collections import OrderedDict
+
+
+DATETIME_FORMAT = "%Y-%m-%d %H:%M"
+
+
+def _construct_base_endpoint(
+    protocol="http", host="127.0.0.1:8080", project_slug="test_dcfr"
+):
+    return f"{protocol}://{host}/nc/{project_slug}/api/v1/"
+
+
+def _merge_objects(list_a, list_b, pk="id", date_field="updated_at"):
+    """Requires objs in list_a|list_b to be JSON dictionaries with a
+    `pk` key and `date_field` in format `2021-10-25 12:01:02`
+    (parser.parse accepts more, use at your discretion).
+
+    Note that the order of the original list is preserved.
+    New pk objects are appended.
+    """
+    merged_dict = OrderedDict()
+
+    for obj in list_a:
+        merged_dict[obj[pk]] = obj
+
+    for obj in list_b:
+        if obj[pk] in merged_dict:
+            # Compare updated_at entries
+            a = merged_dict[obj[pk]]
+            b = obj
+
+            if parser.parse(a[date_field]) < parser.parse(b[date_field]):
+                merged_dict[obj[pk]] = b
+        else:
+            merged_dict[obj[pk]] = obj
+
+    return list(merged_dict.values())
+
+
+def _get_list_from_api(table, row_limit, nc_protocol, nc_host, project_slug, xc_key):
+    get_endpoint = (
+        _construct_base_endpoint(nc_protocol, nc_host, project_slug)
+        + f"{table}?limit={row_limit}"
+    )
+    resp = requests.get(get_endpoint, headers={"xc-auth": xc_key})
+    return resp.json()
+
+
+def merge(
+    table,
+    project_slug,
+    xc_key,
+    nc_protocol,
+    nc_host,
+    row_limit,
+    json_file,
+    object_pk,
+    object_date_field,
+):
+
+    # Start merge
+
+    # Get all table entries as JSON
+    api_tools = _get_list_from_api(
+        table, row_limit, nc_protocol, nc_host, project_slug, xc_key
+    )
+
+    # Read in all existing entries as JSON from disk
+    file_tools = []
+    if os.path.isfile(json_file):
+        with open(json_file, "rb") as f:
+            file_tools = json.load(f)
+
+    # Merge by letting the object with latest `object_date_field` win
+    merged_file_tools = _merge_objects(
+        api_tools, file_tools, object_pk, object_date_field
+    )
+
+    # Write result to disk & API
+
+    # On disk overwrite old file
+    with open(json_file, "w") as f:
+        json.dump(merged_file_tools, f)
+
+    # On API drop all records, and bulk insert
+    bulk_endpoint = _construct_base_endpoint() + f"{table}/bulk"
+
+    # Drop all tools returned by API
+    requests.delete(
+        bulk_endpoint,
+        json=[{object_pk: obj[object_pk]} for obj in api_tools],
+        headers={"xc-auth": xc_key},
+    )
+
+    # Insert all records
+    requests.post(bulk_endpoint, json=merged_file_tools, headers={"xc-auth": xc_key})
+
+
+def _get_star_count(github_url):
+    # Expected URL format: https://github.com/owner/repo[/...]
+    # [...] ending is optional
+    url = urlparse(github_url)
+
+    _, owner, repo_name = url.path.split("/")[:3]
+    repo_info = requests.get("https://api.github.com/repos/" + owner + "/" + repo_name)
+
+    return repo_info.json()["stargazers_count"]
+
+
+def update_stars(
+    table,
+    row_limit,
+    nc_protocol,
+    nc_host,
+    project_slug,
+    xc_key,
+    object_pk,
+    object_date_field,
+    github_column="GitHub URL",
+    github_star_column="GitHub Stars",
+):
+    api_tools = _get_list_from_api(
+        table, row_limit, nc_protocol, nc_host, project_slug, xc_key
+    )
+
+    for tool in api_tools:
+        if (
+            github_column in tool
+            and github_star_column in tool
+            and "http" in tool[github_column]
+        ):
+            try:
+                star_count = _get_star_count(tool[github_column])
+
+                update_endpoint = (
+                    _construct_base_endpoint(nc_protocol, nc_host, project_slug)
+                    + f"{table}/{tool[object_pk]}"
+                )
+
+                # Make a PUT iff star_count changed
+                if tool[github_star_column] != star_count:
+                    requests.put(
+                        update_endpoint,
+                        json={
+                            object_date_field: datetime.datetime.utcnow().strftime(
+                                DATETIME_FORMAT
+                            ),
+                            github_star_column: star_count,
+                        },
+                        headers={"xc-auth": xc_key},
+                    )
+            except:
+                logging.info(
+                    "Failed to get GitHub star count for %s" % tool[github_column]
+                )
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO)
+
+    # Required environment variables
+    table = os.environ.get("NC_TABLE_NAME")
+    project_slug = os.environ.get("NC_PROJECT_SLUG")
+    xc_key = os.environ.get("NC_XC_KEY")
+
+    # Optional environment variables
+    nc_protocol = os.environ.get("NC_PROTOCOL", "http")
+    nc_host = os.environ.get("NC_HOST", "127.0.0.1:8080")
+    row_limit = int(os.environ.get("NC_ROW_LIMIT", 50000))
+    json_file = os.environ.get("JSON_FILE", "tools.json")
+    object_pk = os.environ.get("NC_OBJECT_PK", "id")
+    object_date_field = os.environ.get("NC_OBJECT_DATE_FIELD", "updated_at")
+
+    arg_parser = argparse.ArgumentParser(prog="sync.py")
+    arg_parser.add_argument("action", choices=["merge", "update-stars"])
+    args = arg_parser.parse_args()
+
+    if args.action == "merge":
+        merge(
+            table,
+            project_slug,
+            xc_key,
+            nc_protocol,
+            nc_host,
+            row_limit,
+            json_file,
+            object_pk,
+            object_date_field,
+        )
+    elif args.action == "update-stars":
+        # Note, if the star count is updated, any changes
+        # in the tools.json are ignored (as the API version will
+        # always be newer).
+        update_stars(
+            table,
+            row_limit,
+            nc_protocol,
+            nc_host,
+            project_slug,
+            xc_key,
+            object_pk,
+            object_date_field,
+        )
+    else:
+        raise Exception("Command %s not recognized" % args.action)
